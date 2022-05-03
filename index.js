924dd99bd6a927f4c31c7477dbe4fc1b17a8dbc2
SangYong-Jeong <mmaf@naver.com>
2022-05-04 01:46:09 +0900

    hangman game 진행

--- index.js
+++ index.js
@@ -1,79 +1,126 @@
-/* 
-UI 기획 
-1) 카테고리 선택 기능 page 따로 구현 필요 -> category 선택하는 page 따로 구현
-2) 카테고리 클릭 후에 나오는 화면 구현 (퍼블리싱) -> 완료
-3) button 나와야함 -> innerHTML 이용해서 구현 필요 -> 구현 완료 
-4) 해당 카테고리 나와야하고 -> 이건 나중에 구현 + 답으로 정해져있는 단어는 미리 정해져야한다.
--> 문제 배열이 필요한다. 우선 하나의 문제만 푼다고 가정하고 코딩
--> 해당 문자열의 length 만큼 problem__wrapper__spelling 태그 생성 -> 구현 완료
--> 버튼 클릭시 검증 필요 (클릭 이벤트 구현 완료) -> 맞다면 answer update 해당 index의 spelling update 및 버튼 disabled 줘야한다. (다시 for문을 돌려서 button setting?) + 다시 for 문을 돌려서 problem_wrapper 안에 들어가는 tag 재생성 및 배치 필요 / 틀린 spelling 인 경우에는 canvas에 졸라맨 그려서 해당 hangman 이 나오게 setting -> 총 10번의 기회를 줄 예정이다. -> 구현 완료
--> 틀렸다면 canvas에 졸라맨 생성 및 -> lives 1 감소 기능 구현 필요 -> 구현 완료
--> 틀린 거 click 시에도 버튼에 disabled 걸어줘야 한다..... () -> 구현 완료
--> canvas 졸라맨으로 그려주는 작업필요  -> 구현 완료
--> canvas 이용해서 hangman 그리기 ~ 완성
-
--> 0 되면 gameover 나오게 setting -> 그리고 답이 나오게 setting 및 모든 button 같은 경우에는 disabled 처리 필요
--> 힌트 및 다시풀기 기능 구현 -> 힌트 버튼은 구현 완료
--> hint 버튼 누르면 clue 에 관련 단어의 hint 추가 -> 구현 완료
--> replay 버튼 누르면 전부다 reset 되고 다시 start 새로고침 강제로 발생시킨다? -> 깜빡이는게 ui상 별로일 것같다. 초기화 할 부분만 선정해서 해당부분만 reset 시켜주면 될것 같다. init 하는 부분들을 함수로 뺀 다음 해당 함수를 다시 실행해 reset 해주면 될 것같다. good -> 구현 완료 
--> scss 파일 분리 필요 (완성 되었다 느낄 때)
-
--> 이제 구현해야할 부분 -> category 선택 page 
--> (좀 더 고도화 시킨다면 a, e, i, o, u) 모음은 전부 선택할 수 있게하고 lives는 안깎이게 세팅
--> 하지만 자음은 틀리면 - 1
-
--> 카테고리 page 퍼블리싱 필요
--> 쿼리 스트링 또는 path 이용해서 어떤 category인지 구분 필요 -> method 및 속성 존재하는 지 확인 필요
-
--> 및 간단한 data들 -> firebase 활용해서? -> 추후에.. 전체적인 기능 구현후 그에 맞춰 data 생성 하면 될것같다. ajax 이용 (axios)
-
--> 한 page 안에서 클릭 시 category 선택 되고 back 누르면 다시 category 창 나오게 setting 필요 (작업 우선순위 0순위) + hangman game 부분에서 back 버튼 (category선택으로 돌아갈 수 있는 버튼 필요) -> 다음주에 작업 start 아침에 (+ 주말에 )
-
--> 카테고리 js로 세팅 (init)
-
-*/
 
 // constant
 const categories = ['Animals', 'Home', 'Clothes', 'Food', 'Jobs', 'Sports', 'Countries', 'Colors', 'Transport'];
 
 
-// 문제 같은 경우에는 해당 배열의 순서를 random 으로 섞어서 { problem: string; hint: string } 이 형태로 짜는게 제일 베스트일 것 같다.
-
-// Animals 카테고리로 테스트 진행하면서 개발
 const problem_words = [
   [
-    { problem: 'dog', hint: 'It likes people' },
-    { problem: 'cat', hint: 'It doesn\'t care about people ' }
+    { problem: 'DOG', hint: './img/dog.jpg' },
+    // { problem: 'CAT' },
+    // { problem: 'FROG' },
+    // { problem: 'CHICKEN' },
+    // { problem: 'TURTLE' },
+    // { problem: 'CRAB' },
+    // { problem: 'RABBIT' },
+    // { problem: 'SHARK' },
+    // { problem: 'CROCODILE' },
+    // { problem: 'GIRAFFE' },
   ]
   ,
   [
-    { problem: 'sofa', hint: 'It gives people comfort ' },
+    { problem: 'SOFA' },
+    { problem: 'BED' },
+    { problem: 'COMPUTER' },
+    { problem: 'AIRCONDITIONER' },
+    { problem: 'MIRROR' },
+    { problem: 'BIN' },
+    { problem: 'CHARE' },
+    { problem: 'DOOR' },
+    { problem: 'PICTURE' },
+    { problem: 'FIREPLACE' },
   ],
   [
-    { problem: 'socks', hint: 'It\'s on your feet.' },
+    { problem: 'SOCKS' },
+    { problem: 'TSHIRT' },
+    { problem: 'SUIT' },
+    { problem: 'BELT' },
+    { problem: 'PANTS' },
+    { problem: 'VEST' },
+    { problem: 'GLOVES' },
+    { problem: 'DRESS' },
+    { problem: 'RING' },
+    { problem: 'SKIRT' },
   ],
   [
-    { problem: 'chocolate', hint: 'The food is very sweet.' },
+    { problem: 'CHOCOLATE' },
+    { problem: 'HAMBURGER' },
+    { problem: 'NOODLES' },
+    { problem: 'BAGUETTE' },
+    { problem: 'APPLE' },
+    { problem: 'SANDWICH' },
+    { problem: 'PEAS' },
+    { problem: 'MANGOES' },
+    { problem: 'MUFFIN' },
+    { problem: 'ORANGES' },
   ],
   [
-    { problem: 'doctor', hint: 'The job heals people.' },
+    { problem: 'DOCTOR' },
+    { problem: 'POLICEMAN' },
+    { problem: 'NURSE' },
+    { problem: 'MECHANIC' },
+    { problem: 'ARTIST' },
+    { problem: 'DANCER' },
+    { problem: 'FIREFIGHTER' },
+    { problem: 'HAIRDRESSER' },
+    { problem: 'PILOT' },
+    { problem: 'DRIVER' },
   ],
   [
-    { problem: 'soccer', hint: 'That exercise is kicking a ball with your feet.' },
+    { problem: 'SOCCER' },
+    { problem: 'CYCLING' },
+    { problem: 'WRESTLING' },
+    { problem: 'DIVING' },
+    { problem: 'BASKETBALL' },
+    { problem: 'BASEBALL' },
+    { problem: 'HOCKEY' },
+    { problem: 'BOWLING' },
+    { problem: 'SWIMMING' },
+    { problem: 'BOXING' },
   ],
   [
-    { problem: 'China', hint: 'That country has the most population in the world.' },
+    { problem: 'CHINA' },
+    { problem: 'AMERICA' },
+    { problem: 'AUSTRALIA' },
+    { problem: 'CANADA' },
+    { problem: 'KOREA' },
+    { problem: 'FRANCE' },
+    { problem: 'ENGLAND' },
+    { problem: 'SPAIN' },
+    { problem: 'INDIA' },
+    { problem: 'ITALY' },
   ],
   [
-    { problem: 'blue', hint: 'This color is the color of the sea.' },
+    { problem: 'BLUE' },
+    { problem: 'RED' },
+    { problem: 'YELLOW' },
+    { problem: 'BLACK' },
+    { problem: 'ORANGE' },
+    { problem: 'PURPLE' },
+    { problem: 'BLUE' },
+    { problem: 'GREEN' },
+    { problem: 'WHITE' },
+    { problem: 'GREY' },
   ],
   [
-    { problem: 'boat', hint: 'It\'s moving on the sea.' },
+    { problem: 'BOAT' },
+    { problem: 'BIKE' },
+    { problem: 'CAR' },
+    { problem: 'SCOOTER' },
+    { problem: 'BUS' },
+    { problem: 'TRUCK' },
+    { problem: 'SKATEBOARD' },
+    { problem: 'PLANE' },
+    { problem: 'TRAIN' },
+    { problem: 'HELICOPTER' },
   ]
 ];
 
+// 모음 알파벳
+// a, e, i, o, u
+
 window.onload = function () {
-  const alphabet = ['a', 'b', 'c', 'd', 'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  const collection_alphabet = ['A', 'E', 'I', 'O', 'U'];
+  const consonant_alphabet = ['B', 'C', 'D', 'F','G','H', 'J','K','L','M','N' ,'P','Q','R','S','T','V','W','X','Y','Z'];
 
   // container 
   const header_container = document.querySelector('.header-wrapper');
@@ -81,19 +128,17 @@ window.onload = function () {
   const start_container = document.querySelector('.start-wrapper');
   const category_container = document.querySelector('.category-wrapper');
   const problem_page_container = document.querySelector('.problem-container');
-  const problem_container = document.querySelector('.problem-wrapper');
   const loading_container = document.querySelector('.progress-wrapper');
   const alphabet_container = document.querySelector('.alphabet-wrapper');
-  const canvas_container = document.querySelector('.canvas-wrapper');
-  const button_container = document.querySelector('.button-wrapper');
+  const collection_container = document.querySelector('.collection-alphabet-wrapper');
+  
 
   // wrapper 
-  const start_wrapper = document.querySelector('.start-wrapper__wrap');
   const count_wrapper = document.querySelector('.problem-header-wrapper__count');
   const category_wrapper = document.querySelector('.category-wrapper__wrap');
   const problem_wrapper = document.querySelector('.problem-wrapper__problem');
-  const life_wrapper = document.querySelector('.problem-wrapper__my-lives');
-  const clue_wrapper = document.querySelector('.problem-wrapper__clue');
+  const hint_wrapper = document.querySelector('.problem-wrapper__hint');
+  const hint_img = document.querySelector('.problem-wrapper__img')
   const hangman = document.getElementById('hangman');
   const context = hangman.getContext('2d');
   
@@ -114,13 +159,11 @@ window.onload = function () {
   let chosen_category_problems_now = 0;
   let chosen_problem = '';
   let chosen_problem_word = '';
+  let chosen_problem_hint = '';
 
   let clicked = [];
   let answer = [];
-  let lives = 10;
-
-  let chosen_problem_hint_count = 1;
-  let chosen_problem_hint = '';
+  let lives = 8;
 
   // canvas 
   let draw_array = [
@@ -128,13 +171,11 @@ window.onload = function () {
     drawFrame2,
     drawFrame3,
     drawFrame4,
-    drawHead,
-    drawBody,
-    drawLeftArm,
-    drawRightArm,
-    drawLeftLeg,
-    drawRightLeg
-  ]; // shift method 이용해서 하나하나씩 함수 실행할 예정
+    drawFrame5,
+    drawFrame6,
+    drawFrame7,
+    drawFrame8,
+  ]; 
 
   // event
   start_button.addEventListener('click', onClickStartButton);
@@ -143,7 +184,7 @@ window.onload = function () {
     // start_button 클릭 시 일어나는 햔싱
     if (op === 'back') {
       category_wrapper.innerHTML = '';
-      problem_page_container.style.display = 'none'
+      problem_page_container.style.display = 'none';
       count_wrapper.style.display = 'none';
       header_container.style.display = 'block';
       problem_header_container.style.display = 'none';
@@ -163,7 +204,7 @@ window.onload = function () {
     header_container.style.display = 'none';
     problem_header_container.style.display = 'block'
     category_container.style.display = 'none';
-    problem_page_container.style.display = 'block'
+    problem_page_container.style.display = 'flex'
   }
 
   function shuffleProblem(problems) {
@@ -190,33 +231,72 @@ window.onload = function () {
     count_wrapper.innerHTML = '';
     category_wrapper.innerHTML = '';
     setTimeout(() => {
+        problem_wrapper.innerHTML = '';
         start_container.style.display = 'block';
         loading_container.style.display = 'none'
       }, 2000)
   }
 
     function makeButtons (wrong_spelling) {
-    alphabet_container.innerHTML = '';
-    for (let i = 0; i < alphabet.length; i++) {
+      alphabet_container.innerHTML = '';
+    for (let i = 0; i < consonant_alphabet.length; i++) {
       const list = document.createElement('div');
-      if (answer.includes(alphabet[i]) || lives === 0) list.classList.add('disabled');
+      if (answer.includes(consonant_alphabet[i]) || lives === 0) list.classList.add('disabled');
       else {
-        if (wrong_spelling === alphabet[i] || clicked.includes(alphabet[i]) ) {
-          if (!clicked.includes(alphabet[i])) clicked.push(alphabet[i])
+        if (wrong_spelling === consonant_alphabet[i] || clicked.includes(consonant_alphabet[i]) ) {
+          if (!clicked.includes(consonant_alphabet[i])) clicked.push(consonant_alphabet[i])
           list.classList.add('disabled');
         }
         else list.onclick = onClicKButton;
       }
       list.classList.add('alphabet-wrapper__alphabet');
-      list.innerHTML = alphabet[i];
+      list.innerHTML = consonant_alphabet[i];
       alphabet_container.appendChild(list);
     }
     }
+  
+  function makeCollectionButtons(wrong_spelling) {
+    collection_container.innerHTML = '';
+    for (let i = 0; i < collection_alphabet.length; i++) {
+      const list = document.createElement('div');
+      if (answer.includes(collection_alphabet[i]) || lives === 0) list.classList.add('disabled');
+      else {
+        if (wrong_spelling === collection_alphabet[i] || clicked.includes(collection_alphabet[i])) {
+          if (!clicked.includes(collection_alphabet[i])) clicked.push(collection_alphabet[i])
+          list.classList.add('disabled');
+        }
+        else list.onclick = onClickCollectionButton;
+      }
+      list.classList.add('collection-alphabet-wrapper__alphabet');
+      list.innerHTML = collection_alphabet[i];
+      collection_container.appendChild(list);
+    }
+  }
   
   function onClicKButton (event) {
     validateAlphabet(event.target.innerHTML);
     validateAnswer();
   };
+
+  function onClickCollectionButton(event) {
+    validateCollectionAlphabet(event.target.innerHTML);
+    validateAnswer();
+  }
+
+  function validateCollectionAlphabet (spelling) {
+    if (chosen_problem_word.includes(spelling)) {
+      let index = chosen_problem_word.indexOf(spelling);
+      // index가 여러개 인거 고려해서 while 문 돌려서 해당 index 전부다 뽑아 내야한다.
+      while (index !== -1) {
+        answer[index] = spelling;
+        index = chosen_problem_word.indexOf(spelling, index + 1);
+      }
+      makeCollectionButtons();
+      updateAnswerElement();
+    } else {
+      makeCollectionButtons(spelling);
+    }
+  }
 
   // spelling: string Type
   function validateAlphabet (spelling) {
@@ -230,8 +310,6 @@ window.onload = function () {
       makeButtons();
       updateAnswerElement();
     } else {
-      // 틀린 경우.. canvas에 졸라맨 생성 -> 이건 좀 알아봐야한다.
-      // + 틀린 경우 life-wrapper.innerHTML update 필요
       updateLives();
       makeButtons(spelling);
       const frame = draw_array.shift();
@@ -250,59 +328,108 @@ window.onload = function () {
   problem_wrapper.innerHTML = '';
   for (let i = 0; i < answer.length; i++) {
     problem_wrapper.innerHTML += `
-    <div class="problem-wrapper__spelling">${answer[i]}</div>
+    <div class="problem-wrapper__spelling">${answer[i] === '_' ? '' : answer[i]}</div>
     `;
     }
   }
 
   function updateLives () {
     lives -= 1;
-    if (lives === 0) life_wrapper.innerHTML = `<span style="color: red;">Game over!</spna>`;
-    else life_wrapper.innerHTML = `현재 남은 목숨은 ${lives}입니다!`;
   }
 
   // canvas function
-
+  
   function drawFrame1() {
-    drawCanvas(0, 150, 150, 150);
-  }
-  
-  function drawFrame2() {
-    drawCanvas(20, 150, 20 ,0);
-  }
-  
-  function drawFrame3() {
-    drawCanvas(10, 0, 100, 0);
-  }
-  
-  function drawFrame4() {
-    drawCanvas(75, 0, 75, 20);
-  }
-
-  function drawHead () {
+    context.moveTo(650, 400);
+    context.lineTo(830, 220);
+    context.stroke();
     context.beginPath();
-    context.arc(75, 30, 10, 0, Math.PI * 2, true);
+    context.arc(740, 310, 70, 0, Math.PI * 2, true);
     context.stroke();
   }
   
-  function drawBody () {
-    drawCanvas(75, 40, 75, 70)
+  function drawFrame2() {
+    context.moveTo(1050, 400);
+    context.lineTo(1230, 220);
+    context.stroke();
+    context.beginPath();
+    context.arc(1140, 310, 70, 0, Math.PI * 2, true);
+    context.stroke();
   }
   
-  function drawLeftArm () {
-    drawCanvas(75, 40, 60, 50);
+  function drawFrame3() {
+    context.moveTo(720, 560);
+    context.lineTo(1150, 560);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(725, 550);
+    context.lineTo(725, 750);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(725, 740);
+    context.lineTo(1150, 740);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(1150, 750);
+    context.lineTo(1150, 550);
+    context.stroke();
   }
   
-  function drawRightArm() {
-    drawCanvas(75, 40, 90, 50);
+  function drawFrame4() {
+    context.moveTo(860, 560);
+    context.lineTo(860, 740);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(1000, 550);
+    context.lineTo(1000, 740);
+    context.stroke();
+  }
+
+  function drawFrame5 () {
+    context.beginPath();
+    context.lineWidth = 10;
+    context.moveTo(800, 150);
+    context.lineTo(1000, 150);
+    context.stroke();
+    context.beginPath();
+    context.arc(1015, 150, 20, 0, Math.PI, true);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(800, 100);
+    context.lineTo(1000, 100);
+    context.stroke();
+    context.beginPath();
+    context.arc(1015, 100, 20, 0, Math.PI, true);
+    context.stroke();
+    context.beginPath();
+    context.stroke();
+    context.moveTo(800, 50);
+    context.lineTo(1000, 50);
+    context.stroke();
+    context.beginPath();
+    context.arc(1015, 50, 20, 0, Math.PI, true);
+    context.stroke();
   }
   
-  function drawLeftLeg() {
-    drawCanvas(75, 70, 60, 80);
+  function drawFrame6 () {
+    context.beginPath();
+    context.lineWidth = 20;
+    context.arc(950, 500, 550, 0, Math.PI * 2, true);
+    context.stroke();
   }
   
-  function drawRightLeg() {
-    drawCanvas(75, 70, 90, 80);
+  function drawFrame7() {
+    context.beginPath();
+    context.strokeStyle = '#f00';
+    context.moveTo(0, 0);
+    context.lineTo(2000, 1038);
+    context.stroke();
+  }
+  
+  function drawFrame8() {
+    context.moveTo(2000, 0);
+    context.lineTo(0, 1038);
+    context.stroke();
   }
 
   function drawCanvas (pathFromX, pathFromY, pathToX, pathToY) {
@@ -314,29 +441,36 @@ window.onload = function () {
   function initCanvas () {
     context.beginPath();
     context.strokeStyle = '#fff';
-    context.lineWidth = 2;
+    context.lineWidth = 20;
   }
 
   // hint
-  function showHint () {
-    if (chosen_problem_hint_count > 0) {
-      // 힌트 하나 준다. clue-wrapper update 필요
-      clue_wrapper.innerHTML += `<span style="color: red; margin-left: 1rem;">${chosen_problem_hint}</span>`;
-    }
-    chosen_problem_hint_count -= 1;
+  function showHint() {
+    hangman.style.display = 'none';
+    hint_wrapper.style.display = 'flex';
+    hint_img.src = chosen_problem_hint;
+    setTimeout(() => {
+      hint_wrapper.style.display = 'none';
+      hangman.style.display = 'flex';
+    }, 5000);
   }
 
-  function reset (op) {
-    chosen_problem_hint_count = 1;
-    lives = 10;
-    alphabet_container.innerHTML = '';
-    problem_wrapper.innerHTML = '';
-    clue_wrapper.innerHTML = 'Clue -';
+  function reset (op) {hint_img
+    lives = 8;
     answer = [];
     clicked = [];
-    draw_array = [drawFrame1, drawFrame2, drawFrame3, drawFrame4, drawHead, drawBody, drawLeftArm, drawRightArm, drawLeftLeg, drawRightLeg]
+    draw_array = [
+    drawFrame1,
+    drawFrame2,
+    drawFrame3,
+    drawFrame4,
+    drawFrame5,
+    drawFrame6,
+    drawFrame7,
+    drawFrame8,
+  ]; 
     initGame(op);
-    context.clearRect(0, 0, 400, 400)
+    context.clearRect(0, 0, 2000, 2000)
   }
 
   function initGame(op) {
@@ -355,14 +489,14 @@ window.onload = function () {
     chosen_category_problems_now = chosen_category_problems_count - chosen_category_problems.length;
     // view update
     makeButtons();
+    makeCollectionButtons();
     initCanvas();
     for (let i = 0; i < chosen_problem_word.length; i++) {
       problem_wrapper.innerHTML += `
-      <div class="problem-wrapper__spelling">_</div>
+      <div class="problem-wrapper__spelling"></div>
       `;
       answer.push('_');
     }
-    life_wrapper.innerHTML = `현재 남은 목숨은 ${lives}개 입니다!`;
     count_wrapper.innerHTML = `${chosen_category_problems_now} / ${chosen_category_problems_count}`;
   };
   hint_button.onclick = showHint;
@@ -371,34 +505,3 @@ window.onload = function () {
   modal_button.onclick = ()  => initStart();
 }
 
-    
-// 기본적으로 구현할려고 했던 기능들은 전부 구현해놓은 상태
-
-// 이제 문제 맞춘 경우에 다음 문제로 넘어가는 logic ( 맞은 count + 문제의 총수를 오른쪽 위에 표현하면 좋을 것 같다. ) -> 구현 start -> 변수는 세팅 이제 이걸 html tag에 맞게 뿌려주면 된다. prototype을 통해 화면에 어떻게 나오는 지 대략적으로 파악한 후에 js로 구현 -> 구현 완료
-
-// reset 에서 error 발생.. reset 버튼을 눌렀는데 now가 계속 update 되는 현상이 발생하고 있다.. -> 해결 완료
-
-// 문제 다 풀고 나면 Congraturation Modal 나와서 축하 후 카테고리 선택 화면으로 가는 button 클릭 하면 넘어가게 세팅 -> 일단 modal 창 세팅은 완료 마지막 문제 끝났을 때 모달창 띄워주게 세팅 -> 구현 완료 -> 여기서 Go to Category 버튼 클릭 시 다시 카테고리 선택화면으로 나오게 세팅 ( 중간에 로딩 바 나오게 세팅 ) -> 구현 완료
-
-
-// back button 구현 ( loading bar open ) => Category click 으로 돌아가는 기능 구현 -> start -> 구현 완료
-
-// category 별 문제 setting -> start -> prototype 용으로 간단히 -> 완료
-
-
-// design 적인 측면 좀 잘 잡으면 마무리일 것같다. -> layout 잡아주기
-// 반응형 잡기
-// 중복되는 코드들 수정하고 마무리 
-
-/* 
-design 추가로 잡아야 할 부분 -> cateogry 쪽  
-start 쪽도 심심
-design 느낌 대로 하는중 + problem-header-wrapper 카테고리로 돌아갈 때 display 수정 시키기 ( header-wrapper는 display: block;으로 바꾸기  )
-
-clue 같은 경우 좀 강조되게 clue 라는 거 알려줄 수 있게 세팅
-game over 시 modal 이용해서 게임 오버 되었고 button 클릭 시 다시 카테고리로 보내기
-전체적으로 감싸는 wrapper 하나 만들기
-1차 완성
-
--> 모음 먼저 선택하는 기능 있으면 좋을 것 같다.
-*/
